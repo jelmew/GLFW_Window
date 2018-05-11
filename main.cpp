c99888f5b73d8b3650015081a06016481ec15ae7
jelmer van amen <jelmew@192.168.1.3>
2018-05-11 10:05:11 +0200

    Initialized openGL context

--- main.cpp
+++ main.cpp
@@ -1,29 +1,37 @@
 #include <iostream>
 #include <GLFW/glfw3.h>
 
-static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods){
-    if(key == GLFW_KEY_ESCAPE && action== GLFW_PRESS) {
+static void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods) {
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
         glfwSetWindowShouldClose(window, GLFW_TRUE);
     }
 }
 
 int main() {
     //Initizlize GLFW
-   if(! glfwInit()){
-      return -1;
-   };
+    if (!glfwInit()) {
+        return -1;
+    };
 
-    GLFWwindow* window = glfwCreateWindow(640,480,"MyTitle", nullptr, nullptr);
-    if(!window){
+    GLFWwindow *window = glfwCreateWindow(640, 480, "MyTitle", nullptr, nullptr);
+    if (!window) {
         glfwTerminate();
         return -1;
     }
 
     glfwMakeContextCurrent(window);
 
-    glfwSetKeyCallback(window,key_callback);
+    glfwSetKeyCallback(window, key_callback);
 
-    while(!glfwWindowShouldClose(window)){
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+
+    while (!glfwWindowShouldClose(window)) {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
         glfwPollEvents();
 
     }
